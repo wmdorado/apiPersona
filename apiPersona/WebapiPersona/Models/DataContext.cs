e3b404cd247b339e909026dc1d33ddfb9d766e0b
wmdorado <a2017210277@estudiantes.upsa.edu.bo>
2021-01-25 22:57:25 +0100

    Persona Controller

---
+++ apiPersona/WebapiPersona/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace WebapiPersona.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("Defaultconnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebapiPersona.Models.Person> People { get; set; }
+    }
+}
